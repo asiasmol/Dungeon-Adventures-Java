0d9ec32e5156193fea33f216b37265e8b37763de
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-01-25 11:17:23 +0100

    game - model class and dao interface

---
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameDao.java
@@ -0,0 +1,17 @@
+package com.codecool.dungeoncrawl.dao;
+
+import com.codecool.dungeoncrawl.model.CellModel;
+import com.codecool.dungeoncrawl.model.GameModel;
+
+import java.util.List;
+
+public interface GameDao {
+
+    void add(GameModel game);
+
+    void update(GameModel game);
+
+    GameModel get(int id);
+
+    List<GameModel> getAll();
+}
